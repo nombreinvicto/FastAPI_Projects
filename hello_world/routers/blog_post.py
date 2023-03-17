92abdbe302d98ab4e7797ef9fa7be74724db52d8
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-03-17 01:43:08 -0600

    POST request, path params and query params mix

--- hello_world/routers/blog_post.py
+++ hello_world/routers/blog_post.py
@@ -18,3 +18,12 @@ class BlogModel(BaseModel):
 @router.post("/new")
 def create_blog(blog: BlogModel):
     return f"got blog with title: {blog.title} and content: {blog.content}"
+
+
+@router.post("/new/{id}")
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'data': blog,
+        'version': version
+    }
