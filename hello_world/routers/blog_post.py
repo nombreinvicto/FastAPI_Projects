33bae18cd323cc97a71582e5863b28b1759dab57
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-03-17 02:56:51 -0600

    metadata for parameters used in swagger doc

--- hello_world/routers/blog_post.py
+++ hello_world/routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status, Response, Query
 from pydantic import BaseModel
 from typing import Optional
 
@@ -26,4 +26,16 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'id': id,
         'data': blog,
         'version': version
+    }
+
+
+@router.post("/new/{id}/comment")
+def create_comment(blog: BlogModel,
+                   id: int,
+                   comment_id: int = Query(title='id of the comment',
+                                           description='some description')):
+    return {
+        'blog': blog,
+        'id': id,
+        'comment_id': comment_id
     }
